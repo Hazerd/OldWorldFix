95200f787d9c89ffc8417dc357e895b773dd947e
Hazerdous <dransom96@gmail.com>
2019-04-21 07:02:39 -0400

    Move all user input to Confirmations class

---
+++ OldWorldFix/Confirmation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OldWorldFix
+{
+    public static class Confirmation
+    {
+        internal static bool WorldInvalidConfirmation()
+        {
+            Console.WriteLine("World not found.\r\n" +
+            "Usage: OldWorldFix.exe /path/to/world_directory\r\n" +
+            "'world_directory' is the folder containing 'level.dat'\r\n" +
+            "You can also copy 'OldWorldFix.exe' and 'Substrate.dll' into the world directory and just run 'OldWorldFix.exe'\r\n" +
+            "Please press any key to close the program" );
+            Console.ReadKey();
+            return true;
+        }
+    }
+}
--- OldWorldFix/Program.cs
+++ OldWorldFix/Program.cs
@@ -21,10 +21,7 @@ namespace OldWorldFix
             NbtWorld world = NbtWorld.Open(path);
             if (world == null)
             {
-                Console.WriteLine("World not found.\r\n" +
-                "Usage: OldWorldFix.exe /path/to/world_directory\r\n" +
-                "'world_directory' is the folder containing 'level.dat'\r\n" +
-                "You can also copy 'OldWorldFix.exe' and 'Substrate.dll' into the world directory and just run 'OldWorldFix.exe'");
+                Confirmation.WorldInvalidConfirmation();
                 return;
             }
             Console.WriteLine("Working with world: {0}", world.Level.LevelName);
