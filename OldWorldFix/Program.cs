de4a071d8d2abbbe104db130409a01894e7d3429
Hazerdous <dransom96@gmail.com>
2019-04-20 21:51:04 -0400

    Implement basic world loading

--- OldWorldFix/Program.cs
+++ OldWorldFix/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Substrate;
+using Substrate.Core;
 
 namespace OldWorldFix
 {
@@ -6,7 +8,17 @@ namespace OldWorldFix
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = args.Length == 0 ? "." : string.Join(" ", args);
+            NbtWorld world = NbtWorld.Open(path);
+            if (world == null)
+            {
+                Console.WriteLine("World not found.\r\n" +
+                "Usage: OldWorldFix.exe /path/to/world_directory\r\n" +
+                "'world_directory' is the folder containing 'level.dat'\r\n" +
+                "You can also copy 'OldWorldFix.exe' and 'Substrate.dll' into the world directory and just run 'OldWorldFix.exe'");
+                return;
+            }
+            Console.WriteLine("Working with world: {0}", world.Level.LevelName);
         }
     }
 }
